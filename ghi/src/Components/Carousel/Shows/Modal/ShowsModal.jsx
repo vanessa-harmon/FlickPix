bcb23fb076c7d29a056af31b3f1c4d5e5c7fba85
Andrew <teamhidef@gmail.com>
2023-10-10 14:57:17 -0400

    seen it and watch later buttons added to modals

--- ghi/src/Components/Carousel/Shows/Modal/ShowsModal.jsx
+++ ghi/src/Components/Carousel/Shows/Modal/ShowsModal.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { useState } from "react";
 import {
     Modal,
     ModalOverlay,
@@ -12,27 +11,13 @@ import {
     Button,
     IconButton,
     SimpleGrid,
-    IconButton,
-    SimpleGrid,
 } from "@chakra-ui/react";
 import './ShowModal.css'
 import { NavLink } from "react-router-dom";
 import { MdOutlineLibraryAddCheck, MdOutlineAddToQueue } from "react-icons/md";
-import { MdOutlineLibraryAddCheck, MdOutlineAddToQueue } from "react-icons/md";
 
 function ShowModal({ show, isOpen, onClose }) {
     const imgUrlPrefix = "https://image.tmdb.org/t/p/original/";
-
-    const [seenIt, setSeenIt] = useState(false);
-    const [added, setAdded] = useState(false);
-
-    const handleSeenItClick = () => {
-        setSeenIt(!seenIt);
-    };
-
-    const handleAddClick = () => {
-        setAdded(!added);
-    };
 
     const [seenIt, setSeenIt] = useState(false);
     const [added, setAdded] = useState(false);
