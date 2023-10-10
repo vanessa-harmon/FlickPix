6fc664ea1d14810a95ec74d4c6bd141d343ee617
Andrew <teamhidef@gmail.com>
2023-10-10 14:44:41 -0400

    seen it and watch later buttons added to modals

--- ghi/src/Components/Carousel/Movies/Modal/MovieModal.jsx
+++ ghi/src/Components/Carousel/Movies/Modal/MovieModal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import {
     Modal,
     ModalOverlay,
@@ -8,12 +9,26 @@ import {
     ModalBody,
     ModalCloseButton,
     Button,
+    IconButton,
+    SimpleGrid,
 } from "@chakra-ui/react";
 import './MovieModal.css'
 import { NavLink } from "react-router-dom";
+import { MdOutlineLibraryAddCheck, MdOutlineAddToQueue } from "react-icons/md";
 
 function MovieModal({ movie, isOpen, onClose }) {
     const imgUrlPrefix = "https://image.tmdb.org/t/p/original/";
+
+    const [seenIt, setSeenIt] = useState(false);
+    const [added, setAdded] = useState(false);
+
+    const handleSeenItClick = () => {
+        setSeenIt(!seenIt);
+    };
+
+    const handleAddClick = () => {
+        setAdded(!added); // Toggle the state
+    };
 
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
@@ -31,15 +46,35 @@ function MovieModal({ movie, isOpen, onClose }) {
                     <p>Rating: {movie.vote_average}</p>
                 </ModalBody>
                 <ModalFooter className="modal-footer">
-                    <Button className="modal-button-close" colorScheme="whiteAlpha" mr={3} onClick={onClose}>
-                        Close
-                    </Button>
-                    <NavLink to={`/movies/${movie.id}`}>
-                        <Button colorScheme="whiteAlpha" >More...</Button>
-                    </NavLink>
+                    <SimpleGrid gap={4} p={4} columns={4}>
+                        <IconButton
+                            icon={<MdOutlineLibraryAddCheck />}
+                            colorScheme={seenIt ? "green" : "green"}
+                            variant="outline"
+                            aria-label="Seen It"
+                            onClick={handleSeenItClick}
+                            isActive={seenIt}
+                            isRound={true}
+                        />
+                        <IconButton
+                            icon={<MdOutlineAddToQueue />}
+                            colorScheme={added ? "yellow" : "yellow"}
+                            variant="outline"
+                            aria-label="Add"
+                            onClick={handleAddClick}
+                            isActive={added}
+                            isRound={true}
+                        />
+                        <NavLink to={`/movies/${movie.id}`}>
+                            <Button colorScheme='twitter' variant='outline' borderRadius="24px">More...</Button>
+                        </NavLink>
+                        <Button className="modal-button-close" borderRadius="24px" colorScheme='red' variant='outline' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                    </SimpleGrid>
                 </ModalFooter>
             </ModalContent>
-        </Modal>
+        </Modal >
     );
 }
 
