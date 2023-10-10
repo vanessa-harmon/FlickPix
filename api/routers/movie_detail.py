eb8da53f5c45bc71463c234b4d27f7e829b256f1
Adekunle Adesanoye <adekuns24@gmail.com>
2023-10-10 14:19:28 -0500

    Detail backend complete

---
+++ api/routers/movie_detail.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter
+import requests
+import json
+from keys.keys import TMDB_API_KEY
+
+router = APIRouter()
+
+
+@router.get("/{movie_id}")
+def movie_detail(movie_id: int):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": TMDB_API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    movie_data = json.loads(response.content)
+
+    movie_details = {
+        "title": movie_data.get("original_title"),
+        "imdb_link": movie_data.get("imdb_id"),
+        "year_produced": movie_data.get("release_date"),
+        "synopsis": movie_data.get("overview"),
+        "poster": movie_data.get("poster_path"),
+    }
+
+    service_providers = movie_providers(movie_id)
+    cast_list = movie_cast(movie_id)
+
+    movie_details["service_providers"] = service_providers
+    movie_details["cast_list"] = cast_list
+
+    return movie_details
+
+
+@router.get('/providers')
+def movie_providers(movie_id: int):
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}/watch/providers'
+    headers = {
+        "accept": "application/json",
+        "Authorization": TMDB_API_KEY
+    }
+    response = requests.get(url, headers=headers)
+    providers_data = json.loads(response.content)
+
+    us_providers = []
+
+    if "results" in providers_data and "US" in providers_data["results"]:
+        us_providers = providers_data["results"]["US"]
+
+    return us_providers
+
+
+@router.get('/credits')
+def movie_cast(movie_id: int):
+    url = f'https://api.themoviedb.org/3/movie/{movie_id}/credits'
+    headers = {
+        "accept": "application/json",
+        "Authorization": TMDB_API_KEY
+    }
+    response = requests.get(url, headers=headers)
+    cast_data = json.loads(response.content)
+
+    actors = []
+
+    if "cast" in cast_data:
+        actors = cast_data["cast"]
+
+    return actors
---
+++ api/routers/show_detail.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter
+import requests
+import json
+from keys.keys import TMDB_API_KEY
+
+router = APIRouter()
+
+
+@router.get("/{serie_id}")
+def show_detail(serie_id: int):
+    url = f"https://api.themoviedb.org/3/tv/{serie_id}?language=en-US"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": TMDB_API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    show_data = json.loads(response.content)
+
+    show_details = {
+        "title": show_data.get("original_title"),
+        "imdb_link": show_data.get("imdb_id"),
+        "year_produced": show_data.get("release_date"),
+        "synopsis": show_data.get("overview"),
+        "poster": show_data.get("poster_path"),
+    }
+
+    service_providers = show_providers(serie_id)
+    cast_list = show_cast(serie_id)
+
+    show_details["service_providers"] = service_providers
+    show_details["cast_list"] = cast_list
+
+    return show_details
+
+
+@router.get('/providers')
+def show_providers(serie_id: int):
+    url = f'https://api.themoviedb.org/3/tv/{serie_id}/watch/providers'
+    headers = {
+        "accept": "applications/json",
+        "Authorization": TMDB_API_KEY
+    }
+    response = requests.get(url, headers=headers)
+    providers_data = json.loads(response.content)
+
+    us_providers = []
+
+    if "results" in providers_data and "US" in providers_data["results"]:
+        us_providers = providers_data["results"]["US"]
+
+    return us_providers
+
+
+@router.get('/credits')
+def show_cast(serie_id: int):
+    url = f'https://api.themoviedb.org/3/tv/{serie_id}/credits'
+    headers = {
+        "accept": "application/json",
+        "Authorization": TMDB_API_KEY
+    }
+    response = requests.get(url, headers=headers)
+    cast_data = json.loads(response.content)
+
+    actors = []
+
+    if "cast" in cast_data:
+        actors = cast_data["cast"]
+
+    return actors
