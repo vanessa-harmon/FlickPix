14e594facba7b14dc8a05b095f71491c2c3d8070
Chris Skinner <caskinner7@gmail.com>
2023-10-12 17:30:46 -0500

    stash

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -4,13 +4,11 @@ import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 import Nav from "./Components/Navbar/Navbar.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./Pages/MainPage.jsx";
-import MovieDetail from "./Pages/MovieDetail.js";
-import ShowDetail from "./Pages/ShowDetail.js";
-import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+// import MainPage from "./Pages/MainPage.jsx";
+//import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import MovieDetail from "./Pages/MovieDetail.jsx";
 import ShowDetail from "./Pages/ShowDetail.jsx";
-// import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import MoviePage from "./Pages/MoviePage.jsx";
 import ShowsPage from "./Pages/ShowsPage.jsx";
 
@@ -42,23 +40,23 @@ function App() {
       <BrowserRouter>
         <AuthProvider baseUrl={"http://localhost:8000"}>
           <Nav />
-          {/* <ErrorNotification error={error} /> */}
           <Routes>
-            <Route path="/" element={<MainPage />} />
+            {/* <ErrorNotification error={error} /> */}
+            {/* <Route path="/" element={<MainPage />} /> */}
             {/* <Construct info={launchInfo} /> */}
             <Route path="movies/:id" element={<MovieDetail />} />
             <Route path="shows/:id" element={<ShowDetail />} />
+            <Route path="/movies" element={<MoviePage />} />
+            <Route path="/tv-shows" element={<ShowsPage />} />
+            {/* <Construct info={launchInfo} /> */}
+            <Route path="/movies/:id" element={<MovieDetail />} />
+            <Route path="/tv-shows/:id" element={<ShowDetail />} />
           </Routes>
         </AuthProvider>
-        <Nav />
+        {/* <Nav /> */}
         {/* <ErrorNotification error={error} /> */}
-        <Routes>
-          <Route path="/movies" element={<MoviePage />} />
-          <Route path="/tv-shows" element={<ShowsPage />} />
-          {/* <Construct info={launchInfo} /> */}
-          <Route path="/movies/:id" element={<MovieDetail />} />
-          <Route path="/tv-shows/:id" element={<ShowDetail />} />
-        </Routes>
+        {/* <Routes>
+        </Routes> */}
       </BrowserRouter>
     </div>
   );
