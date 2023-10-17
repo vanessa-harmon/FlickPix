9638217d965988f5705ac56c9be6b41a1958d6b7
Vanessa <vmarieharmon@gmail.com>
2023-10-17 11:10:15 -0500

    watchlater page and journal

--- ghi/src/Pages/WatchLater.jsx
+++ ghi/src/Pages/WatchLater.jsx
@@ -2,36 +2,50 @@ import React, { useEffect, useState } from 'react'
 
 
 function WatchLater() {
-//   fetch data
-  const [watchlist, setWatchList] = useState([]);
+  const [watchLater, setWatchLater] = useState([]);
 
   const fetchData = async () => {
-    const url = ``;
+    const url = `http://localhost:8000/api/watch_later`;
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      setWatchList(data);
+      setWatchLater(data);
     }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4">
-      {watchlist.map((media) => (
-      <div className="col">
-        <div className="card h-100">
-          <img
-            src="http://placekitten.com/220"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Title{/* {media.name} */}</h5>
-            <p className="card-text">{}</p>
-          </div>
-        </div>
+    <div>
+      <div>
+        <h1>Watch Later</h1>
       </div>
-      ))}
+
+      {watchLater.length === 0 ? (
+        <div>
+          <h1>There is currently nothing saved to your watch later list.</h1>
+        </div>
+      ) : (
+        watchLater.map((media) => (
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+              <div className="card h-100">
+                <img
+                  src="http://placekitten.com/220"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Title</h5>
+                  <p className="card-text">{}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 }
--- journals/vanessa_harmon.md
+++ journals/vanessa_harmon.md
@@ -14,4 +14,23 @@ and started up my container I was receieving errors.
 My team debugged and helped me get the correct pulls
 over from Gitlab. I also needed to delete my Node modules
 and completely rebuild.
-After this week I have a better understanding of CSS grid. 
+After this week I have a better understanding of CSS grid.
+
+
+## October 13, 2023
+
+This week, I worked on:
+
+* Carousels for the movie and show recommendations
+
+I fetch the data from the backend. With the help of
+Andrew I was able to then dsiplay this data in a
+carousel on the detail page. So, when a user visits
+a specific show or movie they are provided with a
+list of similar/recommended movies/shows. You can then
+click a recommended show/movie and be redirected to
+the details.
+
+A major a-ha moment was trying to get the page to scroll
+to the top when a recommended show/movie was selected.
+I had to add a click handle to the link as well for this redirect. 
