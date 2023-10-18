f1e5bb66e3a2db7f2391fc60896e17cb7d5b18f7
Chris Skinner <caskinner7@gmail.com>
2023-10-18 18:03:36 -0500

    Random selection feature complete needs styling

--- ghi/src/Components/Randomizer/Randomizer.jsx
+++ ghi/src/Components/Randomizer/Randomizer.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./Randomizer.css";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 
 function Randomizer() {
   const [random, setRandom] = useState([]);
-  const fetchRandom = async () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const imgUrlPrefix = "https://image.tmdb.org/t/p/original/";
+
+  const handleClick = async (event) => {
     const randomUrl = "http://localhost:8000/movies/random";
     const response = await fetch(randomUrl);
     if (response.ok) {
@@ -13,18 +26,54 @@ function Randomizer() {
   };
 
   useEffect(() => {
-    fetchRandom();
+    handleClick();
   }, []);
+
+  useEffect(() => {
+    console.log("RANDOM INSTANCE: ", random);
+  }, [random]);
+
   return (
-    <div className="shuffle-container">
-      <button>
-        <img
-          src="shuffle.png"
-          alt="Shuffle Button"
-          className="shuffle-button"
-        />
-      </button>
-    </div>
+    <>
+      <div className="shuffle-container">
+        <button onClick={onOpen} onClickCapture={handleClick}>
+          <img
+            src="shuffle.png"
+            alt="Shuffle Button"
+            className="shuffle-button"
+          />
+        </button>
+      </div>
+      <div className="modal-shuffle-container">
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent className="random-modal">
+            <ModalHeader>{random.name || random.title}</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <img
+                src={imgUrlPrefix + random.poster_path}
+                alt={random.name || random.title}
+                className="movie-image"
+              />
+              <p>{random.overview}</p>
+              <p>Rating: {random.vote_average}</p>
+            </ModalBody>
+
+            <ModalFooter className="random-footer">
+              <button onClick={onClose}>Close</button>
+              <button onClick={onOpen} onClickCapture={handleClick}>
+                <img
+                  src="shuffle.png"
+                  alt="Shuffle Button"
+                  className="modal-shuffle-button"
+                />
+              </button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </div>
+    </>
   );
 }
 
