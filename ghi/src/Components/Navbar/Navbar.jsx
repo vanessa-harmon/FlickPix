ce9d925ff2f29057eb9be97c5605f91a8058b93b
Valeria <valeria.g.nogales@gmail.com>
2023-10-18 16:00:49 -0400

    Avatar

--- ghi/src/Components/Navbar/Navbar.jsx
+++ ghi/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,45 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
-import SignupModal from "../Authentication/SignupModal";
-import LoginModal from "../Authentication/LoginModal";
+import SignupModal from "../Account/SignupModal";
+import LoginModal from "../Account/LoginModal";
 // import Logout from "../Authentication/Logout";
-import { Tabs, TabList, Tab, TabIndicator, Avatar, Menu, MenuButton, MenuList, MenuItem, Button, ButtonGroup, useDisclosure } from "@chakra-ui/react";
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabIndicator,
+  Avatar,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Wrap,
+  WrapItem,
+  ModalCloseButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Image,
+  Button,
+  ButtonGroup,
+  useDisclosure,
+} from "@chakra-ui/react";
 import "./Navbar.css";
 import SearchBar from "../SearchBar/SearchBar";
 import useToken from "@galvanize-inc/jwtdown-for-react";
+import Pic1 from "../AvatarPics/1.jpeg";
+import Pic2 from "../AvatarPics/2.jpg";
+import Pic3 from "../AvatarPics/3.jpg";
+import Pic4 from "../AvatarPics/4.png";
+import Pic5 from "../AvatarPics/5.jpg";
+import Pic6 from "../AvatarPics/6.png";
+import Pic7 from "../AvatarPics/7.png";
+import Pic8 from "../AvatarPics/8.png";
+import Pic9 from "../AvatarPics/9.png";
+import Pic10 from "../AvatarPics/10.png";
 
 function Nav() {
   const [isOpenLogin, setIsOpenLogin] = useState(false);
@@ -15,6 +48,8 @@ function Nav() {
   const [isOpenSignup, setIsOpenSignup] = useState(false);
   const openSignupModal = () => setIsOpenSignup(true);
   const closeSignupModal = () => setIsOpenSignup(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [avatar, setAvatar] = useState(Pic4);
   const [searchResults, setSearchResults] = useState([]);
 
   const navigate = useNavigate();
@@ -26,6 +61,10 @@ function Nav() {
   };
 
   // const navigate = useNavigate();
+
+  // const handleAvatarChange = (avatar) => {
+  //   avatar();
+  // };
 
   const handleSearch = (query) => {
     if (query) {
@@ -74,6 +113,7 @@ function Nav() {
           <LoginModal isOpenLogin={isOpenLogin} onCloseLogin={closeLoginModal} />
           <SignupModal isOpenSignup={isOpenSignup} onCloseSignup={closeSignupModal} />
           {/* <Logout onClick={handleLogout} /> */}
+
           <ButtonGroup spacing="6">
             <Button colorScheme="cyan" onClick={openSignupModal}>
               Register
@@ -83,14 +123,85 @@ function Nav() {
             </Button>
           </ButtonGroup>
           <Menu>
-            <MenuButton className="avatar-center" as={Avatar} name="Andrew" src="/link.png" cursor="pointer"></MenuButton>
+            <Avatar as={MenuButton} size="lg" bg="teal.300" src={avatar} />
             <MenuList>
-              <MenuItem className="menu-item-black-text">Settings</MenuItem>
+              <MenuItem className="menu-item-black-text" onClick={onOpen}>
+                Avatar Settings
+              </MenuItem>
               <MenuItem className="menu-item-black-text" onClick={handleLogout}>
                 Log Out
               </MenuItem>
             </MenuList>
           </Menu>
+
+          <Modal isOpen={isOpen} onClose={onClose} size="6xl" isCentered>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Choose your Avatar</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Wrap spacing="30px">
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic1} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic1)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic2} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic2)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic3} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic3)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic4} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic4)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic5} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic5)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic6} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic6)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic7} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic7)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic8} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic8)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic9} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic9)} />
+                    </button>
+                  </WrapItem>
+                  <WrapItem>
+                    <button>
+                      <Image src={Pic10} borderRadius="base" objectFit="fill" boxSize="180px" onClick={() => setAvatar(Pic10)} />
+                    </button>
+                  </WrapItem>
+                </Wrap>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Select Avatar
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </nav>
       </header>
     </>
